1581749b98439507696fba6b2a6f1aa38ef39797
EdwardThapelo <edwardthapelo55@gmail.com>
2025-04-25 20:05:19 +0200

    Commented out the protected route

--- products/routes/productRoute.js
+++ products/routes/productRoute.js
@@ -1,27 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { 
-  getAllProducts, 
-  getProductById, 
-  createProduct, 
-  updateProduct, 
-  deleteProduct 
-} = require('../controllers/productController');
-const { protect, adminOnly } = require('../middleware/authMiddleware');
+const {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productController");
+const { protect, adminOnly } = require("../middleware/authMiddleware");
 
 // GET all products
-router.get('/', getAllProducts);
+router.get("/", getAllProducts);
 
 // GET single product by ID
-router.get('/:id', getProductById);
+router.get("/:id", getProductById);
 
 // CREATE new product
-router.post('/', protect, adminOnly, createProduct);
+router.post("/", 
+// protect,
+//  adminOnly,
+  createProduct);
 
 // UPDATE product
-router.put('/:id', protect, adminOnly, updateProduct);
+router.put("/:id", protect, adminOnly, updateProduct);
 
 // DELETE product
-router.delete('/:id', protect, adminOnly, deleteProduct);
+router.delete("/:id", protect, adminOnly, deleteProduct);
 
 module.exports = router;
